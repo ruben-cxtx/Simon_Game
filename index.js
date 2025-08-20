66e1b1c97665e685a32e6f6af9752d78a3106d93
ruben-cxtx <juarez.ruben2606@gmail.com>
2025-08-20 07:18:14 -0500

    added game start and sequence logic

--- index.js
+++ index.js
@@ -1,43 +1,69 @@
 class Game {
     constructor() {
+        this.buttonList = ['green', 'red', 'yellow', 'blue'];
         this.gameButtons = [];
-        this.$greenButton = $("#green")
-        this.$redButton = $("#red")
 
         this.playButtonAudio();
         this.addEventListeners();
-        this.showPressedKey();
+        this.handleUserPress();
+        this.flash();
+        this.addButtonSequence();
+        this.startGame();
     }
 
 
     addEventListeners() {
-        $("body").click((event) => {
-            this.showPressedKey(event);
+        $(".btn").click((event) => {
+            this.handleUserPress(event);
         })
+
+        $(document).one("keydown", (event) => this.startGame());
+
+
     }
 
     playButtonAudio(id) {
+
+        if(!id) return;
         let audio = new Audio(`sounds/${id}.mp3`)
+        audio.currentTime = 0;
         audio.play()
     }
 
-    showPressedKey(event) {
-        const buttonId = event.target.id;
-
-        $(`#${buttonId}`).toggleClass("pressed")
-
-        this.gameButtons.push(`${buttonId}`)
-
-        this.playButtonAudio(buttonId)
-
-        setTimeout(() => {
-            $(`#${buttonId}`).toggleClass("pressed");
-        }, 100);
-
+    addButtonSequence() {
+        const buttonId = this.buttonList[Math.floor(Math.random() * this.buttonList.length)]
+        this.gameButtons.push(buttonId);
+        console.log(this.gameButtons);
+        this.gameButtons.forEach((id, index) => {
+            setTimeout(() => this.flash(id), 600 * index)
+        })
     }
 
+    handleUserPress(event) {
+        const buttonId = event.target.id;
 
+        this.flash(buttonId)
+    }
 
+    startGame(event){
+        $("body").on("keydown", () => {
+            this.addButtonSequence();
+        })
+        $("body").on("keydown", () => {
+            console.log(event);
+        })
+    }
+
+    flash(buttonId){
+        if (this.buttonList.includes(buttonId)) {
+            $(`#${buttonId}`).addClass("pressed");
+
+            this.playButtonAudio(buttonId);
+
+            setTimeout(() => {
+                $(`#${buttonId}`).removeClass("pressed");
+            }, 100);
+        }
+    }
 }
-
 new Game()
